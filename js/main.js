21bad8447e9c4dc4d467ed10513ddd4a20220ecd
Erik Hassan <ERKHSN@ERKHSN.local>
2017-10-30 11:18:48 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,58 @@
 console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "king";
-var cardFour = "queen";
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = [
+{
+rank: 'queen',
+suit: 'hearts',
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: 'queen',
+suit: 'diamonds',
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: 'king',
+suit: 'hearts',
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: 'king',
+suit: 'diamonds',
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
+var cardsInPlay = [];
+
+var checkForMatch = function (if (cardsInPlay[0] === cardsInPlay[1]); {
+console.log("You found a match!");
+} else {
+console.log("Sorry, try again.");
+})
+
+
+
+var flipcard = function(cardId) {console.log("User flipped" + cards[cardId].rank);};
+
+var createBoard = function(for (var i = 0; i < cards.length; i++); {
+
+});
+
+var img = document.createElement('cardElement');
+
+cardElement.setAttribute('src', 'images/back.png');
+
+cardElement.setAttribute('data-id').i
+cardsInPlay.push(cards[cardId])
+
+flipcard = function(2);
+
+if (cardsInPlay.length === 2);  
+else if (cardsInPlay[0] = cardsInPlay[1]); {
+alert ('Lets Get Paid'); };
+else (cardsInPlay.length === 3); {
+alert ('Deal Again') }; 
+
+};
